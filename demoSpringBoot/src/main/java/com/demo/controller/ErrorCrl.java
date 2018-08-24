05e01d37688808ae1824685c7214f7f47d4a1ce9
jia <jia@10.37.129.2>
2018-08-24 14:45:52 +0800

    test

--- demoSpringBoot/src/main/java/com/demo/controller/ErrorCrl.java
+++ demoSpringBoot/src/main/java/com/demo/controller/ErrorCrl.java
@@ -1,23 +1,23 @@
-package com.demo.controller;
-
-import org.springframework.boot.web.servlet.error.ErrorController;
-import org.springframework.stereotype.Controller;
-
-@Controller
-
-public class ErrorCrl implements ErrorController {
-	private final static String ERROR_PATH = "/error";
-
-	// @RequestMapping("/error")
-	// public String handleError() {
-	// // do something like logging
-	// return "error";
-	// }
-
-	@Override
-	public String getErrorPath() {
-		// TODO Auto-generated method stub
-		return "error";
-	}
-
-}
+//package com.demo.controller;
+//
+//import org.springframework.boot.web.servlet.error.ErrorController;
+//import org.springframework.stereotype.Controller;
+//
+//@Controller
+//
+//public class ErrorCrl implements ErrorController {
+//	private final static String ERROR_PATH = "/error";
+//
+//	// @RequestMapping("/error")
+//	// public String handleError() {
+//	// // do something like logging
+//	// return "error";
+//	// }
+//
+//	@Override
+//	public String getErrorPath() {
+//		// TODO Auto-generated method stub
+//		return "error";
+//	}
+//
+//}
